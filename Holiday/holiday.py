7aeb9ed849cfd4c06f1a4145a278a6ced419ce60
Mason.Lin <mason.lin1@hp.com>
2021-01-10 18:04:23 +0800

    holiday done

--- Holiday/holiday.py
+++ Holiday/holiday.py
@@ -1,3 +1,14 @@
-def say_hello():
-    return "Merry Xmax"
+from datetime import date
 
+
+def say_hello():
+    today = get_today()
+    if today.month == 12 and today.day == 25:
+        return "Merry Xmax"
+    else:
+        return "Today Is Not Xmax"
+
+
+def get_today():
+    today = date.today()
+    return today
--- Holiday/test_holiday.py
+++ Holiday/test_holiday.py
@@ -1,5 +1,22 @@
+from datetime import date
+
 import pytest
+
 from Holiday.holiday import say_hello
 
-def test_today_is_xmax():
+
+def test_today_is_xmax(mocker):
+    given_today(mocker, month=12, day=25)
     assert say_hello() == "Merry Xmax"
+
+
+def test_today_is_not_xmax(mocker):
+    given_today(mocker, month=11, day=25)
+    assert say_hello() == "Today Is Not Xmax"
+
+
+def given_today(mocker, month, day):
+    ANY_YEAR = 2019
+    mocker.patch(
+        "Holiday.holiday.get_today", return_value=date(ANY_YEAR, month, day)
+    )
